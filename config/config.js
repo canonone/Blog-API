02fa29f4a4a8d8e324e48ac8db54ede4923d0650
Umoh Christian <45859425+canonone@users.noreply.github.com>
2024-12-22 11:41:37 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const DATABASE_CONNECT_STRING = process.env.DATABASE_CONNECT_STRING;
-const PORT = 8080 || process.env.PORT;
+const PORT = 5000 || process.env.PORT;
 const SECRET = process.env.SECRET;
 
 module.exports = {
